6c1ea18bc396add0e9eee327700af526130fb551
vitorbinhotti <vitor_binhotti@estudante.sesisenai.org.br>
2025-03-26 08:10:18 -0300

    UPDATE - lista finalizada

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ for (let i = 10; i > 0; i--) {
     console.log(i)
 }
 
-// forma com while - professor ensinando
+// Forma com while - professor ensinando
 
 let numero = 10;
 let delay = 0;
@@ -45,3 +45,56 @@ while (numero >= 1) {
 let original = "JavaScript";
 let invertido = original.split("").reverse().join("");
 console.log(invertido);
+
+// Contar caractere
+
+function contaCaractere(palavra) {
+    console.log(palavra.length);
+}
+
+contaCaractere("Professor");
+
+// carro
+
+let carro = {
+    marca: "BMW",
+    modelo: "320i",
+    ano: 2020,
+}
+console.log(carro.modelo)
+
+// Mensagem
+
+function mensagemPersonalizada(nome){
+    console.log("olá, " + nome);
+}
+mensagemPersonalizada(prompt("Digite seu nome"));
+
+// Media
+
+function media(n1,n2,n3){
+    console.log( (n1 + n2 + n3)/3);
+}
+media(10,20,30)
+
+// Condicao
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0) {
+        console.log(i + " - Múltiplo de 3");
+    } else {
+        console.log(i + " - Não múltiplo de 3");
+    }
+}
+
+
+// Verificacao Palavra Palíndroma
+
+function verificarPalindromo(palavra) {
+    palavra = palavra.toLowerCase();
+    let palavraInvertida = palavra.split('').reverse().join('');
+    return palavra === palavraInvertida;
+}
+
+console.log(verificarPalindromo("arara")); 
+console.log(verificarPalindromo("banana"));
